5016480237f77941f1f66815a203e7832e5cd093
Amber Lim <xalim@wisc.edu>
2023-01-10 16:38:59 -0600

    Functionality to plot phonon DOS

--- src/PlotDFT.jl
+++ src/PlotDFT.jl
@@ -6,6 +6,6 @@ using Plots.PlotMeasures
 
 include("DOS.jl")
 export import_DOS_VASP, num_electrons_at_energy, energy_at_electron_ct, plot_DOS, plot_pDOS,
-pDOS_options, adjust_plot
+plot_PHDOS, plot_pPHDOS, pDOS_options, Ha_to_THz, savefig
 
 end
