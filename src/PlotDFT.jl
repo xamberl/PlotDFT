dfe8e77662a1bb9b45d4fb61e7f088ed622ba915
Amber Lim <xalim@wisc.edu>
2023-01-10 16:39:26 -0600

    removed obsolete function

--- src/PlotDFT.jl
+++ src/PlotDFT.jl
@@ -6,6 +6,6 @@ using Plots.PlotMeasures
 
 include("DOS.jl")
 export import_DOS_VASP, num_electrons_at_energy, energy_at_electron_ct, plot_DOS, plot_pDOS,
-plot_PHDOS, plot_pPHDOS, pDOS_options, Ha_to_THz, savefig
+plot_PHDOS, plot_pPHDOS, pDOS_options, Ha_to_THz
 
 end
