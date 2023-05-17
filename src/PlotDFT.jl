fdea020038320bcbfd7613930bb1368fc90a90ef
Amber Lim <xalim@wisc.edu>
2023-05-17 16:31:25 -0500

    export DOSinfo struct

--- src/PlotDFT.jl
+++ src/PlotDFT.jl
@@ -6,6 +6,7 @@ using Electrum
 using PlotlyJS
 
 include("datastructs.jl")
+export DOSinfo
 
 include("DOS.jl")
 export import_DOS_VASP, num_electrons_at_energy, energy_at_electron_ct, plot_DOS, plot_pDOS,
